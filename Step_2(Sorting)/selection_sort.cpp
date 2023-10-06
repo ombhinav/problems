4ed42b815d412bb52542d0cc624b43b69319098c
ombhinav <abhinavgarg0077@gmail.com>
2023-10-06 22:01:40 +0530

    updated

---
+++ Step_2(Sorting)/selection_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<utility>
+using namespace std;
+
+void selection_sort(int arr[],int n){
+    for(int i=0;i<=n-2;i++){
+        int mini = i; //it's an index;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini = j;
+            }
+        }
+        swap(arr[mini],arr[i]);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    selection_sort(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
