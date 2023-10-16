82067685e0ced9ec2bd944021abfea7d6990a059
ombhinav <abhinavgarg0077@gmail.com>
2023-10-16 22:39:44 +0530

    updated

--- (LL)/creation.cpp
+++ (LL)/creation.cpp
@@ -32,6 +32,36 @@ void insertAtHead(Node* &head, int d){
 
 }
 
+void insertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail = temp;
+
+}
+
+void insertAtPosition(Node* &head, Node* &tail, int position, int d){
+    
+    if(position == 1){
+        insertAtHead(head,d);
+        return;
+    }
+    
+    Node* temp = head;
+    for(int i=1;i<position-1;i++){
+        temp = temp -> next;
+    }
+
+    if(temp -> next == NULL){
+        insertAtTail(tail, d);
+        return;
+    }
+
+    Node* nodeToInsert = new Node(d);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+}
+
 int main(){
 
     Node* node1 = new Node(10);
@@ -39,11 +69,16 @@ int main(){
     cout << node1 -> next << endl;
 
     Node* head = node1;
-    insertAtHead(head,11);
+    Node* tail = node1;
+    insertAtTail(tail,11);
+    // print(head);
+    insertAtTail(tail, 12);
+    // print(head);
+    insertAtTail(tail, 14);
     print(head);
-    insertAtHead(head, 12);
+
+    insertAtPosition(head,tail,5,16);
     print(head);
-    insertAtHead(head, 14);
-    print(head);
+    
     return 0; 
 }
